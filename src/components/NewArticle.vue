51219c78456875acb1408bdffae21c681046c3d1
Chris <liuchao@fmc.com>
2018-10-27 15:07:04 +0800

    修改表单

--- src/components/NewArticle.vue
+++ src/components/NewArticle.vue
@@ -13,30 +13,16 @@
       </el-form-item>
       <el-form-item label="Tags">
         <el-col :span="6">
-          <el-tag
-          :key="tag"
-          v-for="tag in dynamicTags"
-          closable
-          :disable-transitions="false"
-          @close="handleClose(tag)">
-          {{tag}}
-        </el-tag>
-        <el-input
-          class="input-new-tag"
-          v-if="inputVisible"
-          v-model="inputValue"
-          ref="saveTagInput"
-          size="small"
-          @keyup.enter.native="handleInputConfirm"
-          @blur="handleInputConfirm"
-        >
-</el-input>
-<el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
+          <el-tag :key="tag" v-for="tag in form.dynamicTags" closable :disable-transitions="false" @close="handleClose(tag)">
+            {{tag}}
+          </el-tag>
+          <el-input class="input-new-tag" v-if="form.inputVisible" v-model="form.inputValue" ref="saveTagInput" size="small" @keyup.enter.native="handleInputConfirm" @blur="handleInputConfirm"></el-input>
+          <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
         </el-col>
       </el-form-item>
       <el-form-item>
         <el-col>
-          <mavon-editor v-model="form.content" subfield="false" boxShadow="true" defaultOpen="edit" placeholder="input"/>
+          <mavon-editor v-model="form.content" :subfield="false" :boxShadow="false" defaultOpen="edit"/>
         </el-col>
       </el-form-item>
       <el-form-item>
@@ -54,12 +40,13 @@ export default {
     return {
       url: process.env.BASE_URL,
       form: {
-          title: '',
-          datetime: new Date(),
-          dynamicTags: ['标签一', '标签二', '标签三'],
-          inputVisible: false,
-          content: ''
-        }
+        title: '',
+        datetime: new Date(),
+        dynamicTags: ['标签一', '标签二', '标签三'],
+        inputVisible: false,
+        inputValue: '',
+        content: ''
+      }
     };
   },
   methods: {
@@ -68,20 +55,20 @@ export default {
     },
 
     handleClose(tag) {
-      this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
+      this.form.dynamicTags.splice(this.form.dynamicTags.indexOf(tag), 1);
     },
 
     showInput() {
       this.inputVisible = true;
       this.$nextTick(_ => {
-        this.$refs.saveTagInput.$refs.input.focus();
+        this.$refs.form.saveTagInput.$refs.input.focus();
       });
     },
 
     handleInputConfirm() {
       let inputValue = this.inputValue;
       if (inputValue) {
-        this.dynamicTags.push(inputValue);
+        this.form.dynamicTags.push(inputValue);
       }
       this.inputVisible = false;
       this.inputValue = '';
